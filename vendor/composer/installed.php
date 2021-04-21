f35a1a8efe39cad49e9e11e251351cc1399e6051
Wilson Ramirez Zuluaga <wilson.rz@gmail.com>
2021-04-21 11:24:05 -0500

    Envio3

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '0ab93339f6ca71273f6c796604d62336ecbddcef',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '0ab93339f6ca71273f6c796604d62336ecbddcef',
+    ),
+  ),
+);

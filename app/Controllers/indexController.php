8d77618100efe7271dbaf11fb2047183a110d92b
Wilson Ramirez Zuluaga <wilson.rz@gmail.com>
2021-06-23 11:15:07 -0500

    23 de Junio

--- app/Controllers/indexController.php
+++ app/Controllers/indexController.php
@@ -11,11 +11,38 @@ class indexController extends Controller{
    
     public function index(){
         $model = new Model();
-        $users = User::all(); 
-        print_r($users);
+        $users = User::count();
+        echo $users;
+        //Insertar Datos en la DB_DATABASE
+        $newUser = new User();//crear la instancia
+        $newUser->firstname = 'Migel';
+        $newUser->lastname ='Arcacio';
+        $newUser->email= 'macr@gmail.com';
+        $newUser->password = 'werwerwerwer';
+        //$newUser->save();
+        //print_r($users);
         $this->view->render();
     }
     public function mensaje(){
        $this->view->render('home');
+    }
+    public function password(){
+        echo $pass = "wramirez";//Solo tiene caracteres, longitud
+        echo "<br>";
+        echo "Base 64: " . base64_encode($pass);
+        echo "<br>";
+        
+        echo "<br>";
+        $options = [
+            'cost' => 12,
+        ];
+        echo $halt = password_hash($pass, PASSWORD_BCRYPT, $options);
+        echo "<br>";
+        $passwordUser = "wramirez";
+        if(password_verify($passwordUser, $halt)){
+            echo "La contraseña es Correcta";
+        }else{
+            echo "La contraseña  Nooo es Correcta";
+        }
     }
 }
